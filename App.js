3709051e4be2812fe082c226cb4b9aba6c2d6590
morshedul-antor <antor.morshedul@gmail.com>
2023-03-11 13:12:53 +0600

    styles: style add from tailwind

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import tw from './src/library/tailwind'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={tw`flex-1 justify-center items-center bg-deep_primary`}>
+            <Text style={tw`log_btn`}>HEALTHx</Text>
+            <StatusBar style="auto" />
+        </View>
+    )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
