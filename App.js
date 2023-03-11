4edbf4b3d51c89e9c7c46bcc760e833f5630af09
morshedul-antor <antor.morshedul@gmail.com>
2023-03-11 17:12:47 +0600

    add: components

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
-import { Home } from './src/components'
+import { View } from 'react-native'
+import { Home, Login } from './src/components'
 import { UserNameProvider } from './src/contexts/userName'
+import tw from './src/library/tailwind'
 
 export default function App() {
     return (
         <UserNameProvider>
-            <StatusBar style="auto" />
-            <Home />
+            {/* main view part */}
+            <View style={tw`${styles.root}`}>
+                <StatusBar style="auto" />
+                <Login />
+                {/* <Home /> */}
+            </View>
+            {/* end */}
         </UserNameProvider>
     )
+}
+
+const styles = {
+    root: 'w-full h-full mt-[40px]',
 }
