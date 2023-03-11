44f40ef855fa39c796d94d91f3d71912ef7299f5
morshedul-antor <antor.morshedul@gmail.com>
2023-03-11 15:43:56 +0600

    add: home component

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
-import tw from './src/library/tailwind'
+import { Home } from './src/components'
+import { UserNameProvider } from './src/contexts/userName'
 
 export default function App() {
     return (
-        <View style={tw`flex-1 justify-center items-center bg-deep_primary`}>
-            <Text style={tw`log_btn`}>HEALTHx</Text>
+        <UserNameProvider>
             <StatusBar style="auto" />
-        </View>
+            <Home />
+        </UserNameProvider>
     )
 }
