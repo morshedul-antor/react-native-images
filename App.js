c8701c67b2bef6c44c14bdb23aa0e07bc2446724
morshedul-antor <antor.morshedul@gmail.com>
2023-03-12 14:21:50 +0600

    feat: navigator and stack screen add for navigation

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 import { View } from 'react-native'
 import { Home, Login } from './src/components'
 import { UserNameProvider } from './src/contexts/userName'
 import tw from './src/library/tailwind'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
     return (
@@ -10,8 +14,12 @@ export default function App() {
             {/* main view part */}
             <View style={tw`${styles.root}`}>
                 <StatusBar style="auto" />
-                <Login />
-                {/* <Home /> */}
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen name="login" component={Login} options={{ headerShown: false }} />
+                        <Stack.Screen name="home" component={Home} options={{ headerShown: false }} />
+                    </Stack.Navigator>
+                </NavigationContainer>
             </View>
             {/* end */}
         </UserNameProvider>
