a0a8c9fd3eba615cb5b316255f234be7882fab91
morshedul-antor <antor.morshedul@gmail.com>
2023-03-12 10:26:38 +0600

    feat: checkbox add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,33 +1,17 @@
-import { useState, useContext } from 'react'
-import { View, Text, TextInput } from 'react-native'
+import { useContext } from 'react'
+import { View, Text } from 'react-native'
 import UserName from '../../contexts/userName'
 import tw from '../../library/tailwind'
 
 export default function Home() {
     const { userName, setUserName } = useContext(UserName)
 
-    const [identifier, setIdentifier] = useState('')
-    // const [password, setPassword] = useState('')
-
     const handleSubmit = () => {
-        if (identifier === 'antor') {
-            setUserName('Morshedul Bari Antor')
-        } else {
-            setUserName('Wrong Name!')
-        }
+        setUserName('Antor')
     }
 
     return (
         <View style={tw`flex-1 justify-center items-center bg-deep_primary`}>
-            <TextInput
-                style={tw`bg-white p-[10px] rounded`}
-                keyboardType="default"
-                autoCapitalize="none"
-                onChangeText={(e) => setIdentifier(e)}
-                defaultValue={identifier}
-                placeholder=""
-                required
-            />
             <Text style={tw`log_btn`} onPress={handleSubmit}>
                 HEALTHx
             </Text>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,9 @@
-import { useState, useContext } from 'react'
+import Checkbox from 'expo-checkbox'
+import { useState, useEffect, useContext } from 'react'
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import UserName from '../../contexts/userName'
 import tw from '../../library/tailwind'
+import { getSavedLogin, setSavedLogin } from '../../utils/remember'
 import { InputIcon, InputPass, OuterHeader, HxLogoIcon } from '../Resource'
 
 export default function Login() {
@@ -9,8 +11,13 @@ export default function Login() {
 
     const [identifier, setIdentifier] = useState('')
     const [password, setPassword] = useState('')
-    // const [rememberMe, setRememberMe] = useState(false)
+    const [rememberMe, setRememberMe] = useState(false)
     const [isOpen, setIsOpen] = useState(true)
+
+    // login save
+    useEffect(() => {
+        getSavedLogin(setIdentifier, setPassword, setRememberMe)
+    }, [])
 
     const handleSubmit = () => {
         if (identifier === '01927194591' && password === '1234') {
@@ -50,11 +57,24 @@ export default function Login() {
                 </InputPass>
             </View>
 
+            <View style={tw`${styles.remember_forget}`}>
+                <View style={tw`${styles.checked}`}>
+                    <Checkbox
+                        value={rememberMe}
+                        onValueChange={(e) => setRememberMe(e)}
+                        color={rememberMe ? '#419cd9' : undefined}
+                    />
+                    <Text style={tw`text-dark_grey`}>Remember Me</Text>
+                </View>
+                <Text style={tw`${styles.forgot_password}`}>Forgot Password?</Text>
+            </View>
+
             <View style={tw`mt-[30px]`}>
                 <TouchableOpacity style={tw`mx-auto`}>
                     <Text
                         style={tw`log_btn`}
                         onPress={() => {
+                            setSavedLogin(rememberMe, identifier, password)
                             handleSubmit()
                         }}>
                         Login
@@ -73,7 +93,7 @@ export default function Login() {
 
 const styles = {
     remember_forget: 'flex-row justify-between items-center mt-[18px]',
-    checked: 'flex-row items-center',
+    checked: 'flex-row items-center gap-2',
     forgot_password: 'text-secondary font-700',
     register_now: 'text-secondary font-700',
     have_account: 'text-center mt-[16px] text-grey font-500',
