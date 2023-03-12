0eb4c1bcc9b87826573d3e65e370a1f17617c4de
morshedul-antor <antor.morshedul@gmail.com>
2023-03-12 17:23:30 +0600

    feat: resusable component add

--- src/components/Resource/Logo/HxLogoText.jsx
+++ src/components/Resource/Logo/HxLogoText.jsx
@@ -1,12 +1,16 @@
 import { View, Image } from 'react-native'
 import tw from '../../../library/tailwind'
 
-export default function HxLogoText({ position }) {
+export default function HxLogoText({ position, color }) {
     return (
         <View style={tw`flex-row justify-${position}`}>
             <Image
                 style={tw`${styles.img}`}
-                source={require('../../../assets/images/logo/hx-blue-fit.png')}
+                source={
+                    color === 'white'
+                        ? require('../../../assets/images/logo/hx-white-fit.png')
+                        : require('../../../assets/images/logo/hx-blue-fit.png')
+                }
                 resizeMode="cover"
             />
         </View>
--- src/components/Resource/index.js
+++ src/components/Resource/index.js
@@ -3,5 +3,6 @@ import InputIcon from './InputField/InputIcon'
 import InputPass from './InputField/InputPass'
 import HxLogoIcon from './Logo/HxLogoIcon'
 import HxLogoText from './Logo/HxLogoText'
+import Wrapper from './Wrapper/Wrapper'
 
-export { InputIcon, InputPass, OuterHeader, HxLogoText, HxLogoIcon }
+export { Wrapper, InputIcon, InputPass, OuterHeader, HxLogoText, HxLogoIcon }
