a3fdb694e20dbf35005b054e0d7b7a68725dc0ca
morshedul-antor <antor.morshedul@gmail.com>
2023-03-13 16:58:09 +0600

    fix: header shown option

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView, Platform } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import { UserNameProvider } from './src/contexts/userName'
+import tw from './src/library/tailwind'
 import { HomeScreen } from './src/screens'
+import { Android } from './src/utils/platform'
 
 const Stack = createNativeStackNavigator()
 
@@ -11,15 +13,24 @@ export default function App() {
     return (
         <UserNameProvider>
             {/******************** main view part ***********************/}
-            <SafeAreaView style={{ flex: 1, marginTop: Platform.OS === 'android' ? 40 : 10 }}>
+            <SafeAreaView style={tw`${style.root}`}>
                 <StatusBar style="auto" />
                 <NavigationContainer>
-                    <Stack.Navigator>
-                        <Stack.Screen name="home" component={HomeScreen} options={{ headerShown: false }} />
+                    <Stack.Navigator
+                        screenOptions={{
+                            headerShown: false,
+                        }}>
+                        {/************** screen **************/}
+                        <Stack.Screen name="home" component={HomeScreen} />
+                        {/*************** end ***************/}
                     </Stack.Navigator>
                 </NavigationContainer>
             </SafeAreaView>
             {/******************** end view part ***********************/}
         </UserNameProvider>
     )
+}
+
+const style = {
+    root: `flex-1 bg-white pt-[${Android ? 30 : 10}px]`,
 }
